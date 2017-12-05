5c106f1f77fbcd38fc8768d5bcbea7417a7e9edf
julie hawkins <julabi@gmail.com>
2017-12-04 21:39:08 -0700

    builds basic button and header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header/Header';
+import Button from './Button/Button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Swapi Box</h1>
+        <Header />
+        <Button />
       </div>
     );
   }
