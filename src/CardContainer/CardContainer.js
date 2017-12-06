0809f811b25456121e0c78472df645e42615bd1f
julie hawkins <julabi@gmail.com>
2017-12-06 16:35:02 -0700

    adds basic card and card container styles

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
-// import './CardContainer.css';
+import './CardContainer.css';
 
 const CardContainer = (props) => {
   let mappedCards = [];
