aef607a92df239a8eeb146f3613a9cb2b89b45cf
julie hawkins <julabi@gmail.com>
2017-12-08 09:32:31 -0700

    lints and proptypes Controls

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 import './Controls.css';
 
 const Controls = (props) => {
   return (
     <div className='Controls'>
-      <Button name='People' displayCards={props.displayCards} displaying={props.displaying}/>
-      <Button name='Planets' displayCards={props.displayCards} displaying={props.displaying}/>
-      <Button name='Vehicles' displayCards={props.displayCards} displaying={props.displaying}/>
+      <Button 
+        name='People' 
+        displayCards={props.displayCards} 
+        displaying={props.displaying} />
+      <Button 
+        name='Planets' 
+        displayCards={props.displayCards} 
+        displaying={props.displaying} />
+      <Button 
+        name='Vehicles' 
+        displayCards={props.displayCards} 
+        displaying={props.displaying} />
     </div>
-  )
-}
+  );
+};
+
+Controls.propTypes = {
+  displaying: PropTypes.string,
+  displayCards: PropTypes.func
+};
 
 export default Controls;
