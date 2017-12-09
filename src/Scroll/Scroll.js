5c43aac53f905929935e1a0de6e5ffb574283c14
julie hawkins <julabi@gmail.com>
2017-12-09 11:06:25 -0700

    moves numerals into apiCalls.js, into fetchFilm

--- src/Scroll/Scroll.js
+++ src/Scroll/Scroll.js
@@ -3,23 +3,15 @@ import PropTypes from 'prop-types';
 import './Scroll.css';
 
 const Scroll = ({currentFilm}) => {
-  const numerals = {
-    1: 'I',
-    2: 'II',
-    3: 'III',
-    4: 'IV',
-    5: 'V',
-    6: 'VI',
-    7: 'VII'
-  };
-
   return (
     <div className='scroll-container'>
     {
       currentFilm.crawlText && 
       <div className='Scroll'>
-        <p className='intro'>A long time ago in a galaxy far, far away....</p>
-        <h3>Episode {numerals[currentFilm.episodeNum]}</h3>
+        <p className='intro'>
+          A long time ago in a galaxy far, far away....
+        </p>
+        <h3>Episode {currentFilm.episodeNum}</h3>
         <h3>{currentFilm.title}</h3>
         <p className='scroll-text'>{currentFilm.crawlText}</p>
         <p>Released: {currentFilm.releaseDate}</p>
