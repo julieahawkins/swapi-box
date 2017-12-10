1e3cb7c4c3be29cfbd38a0dc7e5aeaebec2a187a
julie hawkins <julabi@gmail.com>
2017-12-09 17:02:24 -0700

    adds cardContainer tests for mappedCards, choose category and no favs

--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -3,9 +3,42 @@ import { shallow } from 'enzyme';
 import CardContainer from './CardContainer';
 
 describe('CardContainer test', () => {
+  let renderedCardContainer;
+
+  beforeEach(() => {
+    renderedCardContainer = shallow(<CardContainer 
+                                      cards={[]}
+                                      displaying={null} />);
+  })
+
   it('should render correctly', () => {
-    const renderedCardContainer = shallow(<CardContainer />);
 
     expect(renderedCardContainer.find('.CardContainer').length).toEqual(1);
   });
+
+  it('should render a choose category message if nothing has been selected to display', () => {
+    expect(renderedCardContainer.find('h1').text()).toEqual('Choose a Category');
+  })
+
+  it('should render 3 cards if passed an array whose length is 3', () => {
+    const mockCards = [
+      { name: 'Luke Skywalker', info: {cardFav: false} },
+      { name: 'R2-D2', info: {cardFav: false} },
+      { name: 'Darth Vader', info: {cardFav: false} }
+    ];
+
+    renderedCardContainer = shallow(<CardContainer 
+                                      cards={mockCards} 
+                                      displaying={'people'}/>);
+
+    expect(renderedCardContainer.find('h1').length).toEqual(0);
+    expect(renderedCardContainer.find('Card').length).toEqual(3);
+  })
+
+  it('should render a no favorites message if the props displaying === favorites and there are no favorited cards', () => {
+    renderedCardContainer = shallow(<CardContainer 
+                                      cards={[]} 
+                                      displaying={'favorites'}/>);
+    expect(renderedCardContainer.find('h1').text()).toEqual('You have no favorites... COLLECT SOME!')
+  })
 });
