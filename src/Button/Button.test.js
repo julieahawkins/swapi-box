4928a2688af5f9a3eebc9df3ef242d479f9973e0
julie hawkins <julabi@gmail.com>
2017-12-09 18:18:59 -0700

    adds jest to env eslintconfig, lints all tests, moves errorStatus into state

--- src/Button/Button.test.js
+++ src/Button/Button.test.js
@@ -11,12 +11,12 @@ describe('Button test', () => {
     mockDisplayCards = jest.fn();
     mockUpdateFavorites = jest.fn();
 
-    renderedButton = shallow(<Button 
-                                    name={'People'}
-                                    displaying={'people'}
-                                    displayCards={mockDisplayCards} />);
-
-
+    renderedButton = shallow(
+      <Button 
+        name={'People'}
+        displaying={'people'}
+        displayCards={mockDisplayCards} />
+    );
   });
 
   it('should render a button with the class of Button', () => {
@@ -25,24 +25,26 @@ describe('Button test', () => {
     expect(renderedButton.find('.Button').length).toEqual(1);
   });
 
-  it('should call the correct passed function with the correct props in Controls', () => {
+  it('should call the correct func with the correct props in Controls', () => {
 
     renderedButton.find('button').simulate('click');
 
     expect(mockDisplayCards).toHaveBeenCalled();
     expect(mockDisplayCards.mock.calls.length).toEqual(1);
-  })
+  });
 
-  it('should call the correct passed function with the correct props in Card', () => {
-    renderedButton = shallow(<Button 
-                                    name={''}
-                                    updateFavorites={mockUpdateFavorites} />);
+  it('should call the correct func with the correct props in Card', () => {
+    renderedButton = shallow(
+      <Button 
+        name={''}
+        updateFavorites={mockUpdateFavorites} />
+    );
 
     renderedButton.find('button').simulate('click');
 
     expect(mockUpdateFavorites).toHaveBeenCalled();
     expect(mockUpdateFavorites.mock.calls.length).toEqual(1);
-  })
+  });
 
   it('should have a className of "selected" if is selected', () => {
 
@@ -50,10 +52,12 @@ describe('Button test', () => {
   });
 
   it('should have a className of "favorite" if is on a favorited Card', () => {
-    renderedButton = shallow(<Button 
-                                    name={''}
-                                    cardFav={true}
-                                    updateFavorites={mockUpdateFavorites} />);
+    renderedButton = shallow(
+      <Button 
+        name={''}
+        cardFav={true}
+        updateFavorites={mockUpdateFavorites} />
+    );
 
     expect(renderedButton.is('.favorite')).toEqual(true);
   });
